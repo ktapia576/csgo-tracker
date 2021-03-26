ae8a9b698fd846023c4cf46af84ed3ce215a694a
ktapia576 <ktapia576@hotmail.com>
2021-03-26 02:22:29 -0400

    intialize

---
+++ README.md
@@ -0,0 +1,2 @@
+# csgo-tracker
+A repo for a web app to show player stats for a searched player in CSGO
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
