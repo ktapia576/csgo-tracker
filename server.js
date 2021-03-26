98d84754f6ad9ac569f51e379dacef92f73ef0a8
ktapia576 <ktapia576@hotmail.com>
2021-03-26 02:31:33 -0400

    update to env config file and change npm cmds to start server

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ dotenv.config({ path: './config.env'});
 
 const app = express();
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
