25131ac55a43bb1ea260ff32b288d12a4c66a20c
ktapia576 <ktapia576@hotmail.com>
2021-03-26 18:42:34 -0400

    move config up in scope

--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const config = {
+    headers: {
+        'TRN-Api-Key': process.env.TRACKER_API_KEY
+    }
+}
+
 router.get('/:profileIdentifier', async (req, res) => {
     try {
-        const config = {
-            headers: {
-                'TRN-Api-Key': process.env.TRACKER_API_KEY
-            }
-        }
-
         const { profileIdentifier } = req.params;
 
         const response = await axios.get(`${process.env.TRACK_API_URL}/profile/steam/${profileIdentifier}`, config);
