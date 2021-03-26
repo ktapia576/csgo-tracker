99d1eb7a8f6dc9e5057d9a18600a9f1df5d6a397
ktapia576 <ktapia576@hotmail.com>
2021-03-26 18:14:23 -0400

    Add axios and make api call with respons json

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/:profileIdentifier', (req, res) => {
-    console.log(`Profile Identifier: ${req.params.profileIdentifier}`)
-    res.send('Hello')
+router.get('/:profileIdentifier', async (req, res) => {
+    try {
+        const config = {
+            headers: {
+                'TRN-Api-Key': process.env.TRACKER_API_KEY
+            }
+        }
+
+        const { profileIdentifier } = req.params;
+
+        const response = await axios.get(`${process.env.TRACK_API_URL}/profile/steam/${profileIdentifier}`, config);
+
+        res.send(response.data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Server Error'
+        });
+    }
 });
 
 module.exports = router
